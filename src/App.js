b57ccfd9686f8eef2997706c61b9abb87d9857ae
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-07-17 11:39:32 +0200

    Create GuestList component
    Extract the HTML from main component, create a new component in a component directory and paste the HTML in that one.
    
    Export that component and import it in main component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GuestList from './components/GuestList';
 import './App.css';
 
 class App extends Component {
@@ -54,32 +55,7 @@ class App extends Component {
               </tr>
             </tbody>
           </table>
-          <ul>
-            <li className="pending"><span>Safia</span></li>
-            <li className="responded"><span>Iver</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li className="responded">
-              <span>Corrina</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li>
-              <span>Joel</span>
-              <label>
-                <input type="checkbox" /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-          </ul>
+          <GuestList />
         </div>
       </div>
     );
