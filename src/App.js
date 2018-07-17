0e2dac1bd6cb86c9ece7d0bb6a10da8678ebc1d6
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-07-17 11:23:46 +0200

    Initialize the State

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Marjolaine',
+        isConfirmed: false
+      },
+      {
+        name: 'Jean-Yves',
+        isConfirmed: true
+      }
+    ]
+  }
+
+  getTotalInvited = () => this.state.guests.length;
+  // getAttendingGuest = () =>
+  // getUnconfirmedGuests = () =>
+
   render() {
     return (
       <div className="App">
